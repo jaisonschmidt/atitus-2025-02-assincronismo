68fae92d1ff4b89e9ff695a7b76fa71ffe2241af
Jaison <jaisonschmidt@gmail.com>
2025-09-19 20:03:45 -0300

    Refactor time retrieval and formatting functions for improved clarity

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-// como eu capturo a hora, minutos e segundos 
-// do computador do usuário?
-
-const dataEhora = new Date();
-
-console.log("Hora", dataEhora.getHours());
-console.log("Minutos", dataEhora.getMinutes());
-console.log("Segundos", dataEhora.getSeconds());
-
-// função de normalização
-// Crie uma função que recebe um número
-// se este número for menor que 10, retorna o numero com 0 antes
-// ex: 1 -> 01, 7 -> 07, 10 -> 10
 function normalizaNumeros(numero) {
     return (numero < 10) ? "0" + numero : numero;
+}
+
+function retornaDataHora() {
+    const dataEhora = new Date();
+
+    const hora = normalizaNumeros(dataEhora.getHours());
+    const minutos = normalizaNumeros(dataEhora.getMinutes());
+    const segundos = normalizaNumeros(dataEhora.getSeconds());
+
+    return `${hora}:${minutos}:${segundos}`;
 }
